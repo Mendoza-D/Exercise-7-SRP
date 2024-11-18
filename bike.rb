ee8ea15106b2c4ec708f3610117bfe6e6f9b1ac0
Devin <mendozde@oregonstate.edu>
2024-11-17 21:58:35 -0800

    complete in a single commit

--- bike.rb
+++ bike.rb
@@ -1,39 +1,51 @@
 # Bike
 
 class Bike
+  attr_accessor :id, :color, :price, :weight, :rented, :cargo_storage
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
-
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, cargo_storage, weight = STANDARD_WEIGHT, rented = false)
     @id = id
     @color = color
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
+    @cargo_storage = cargo_storage
   end
 
   def rent!
     self.rented = true
   end
 
-  def add_cargo(item)
-    self.cargo_contents << item
-  end
-
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-  end
-
   def pannier_capacity
-    MAX_CARGO_ITEMS
+    cargo_storage.max_capacity
   end
 
   def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+    cargo_storage.remaining_capacity
+  end
+end
+
+class CargoStorage
+  attr_reader :cargo_contents, :max_capacity
+
+  def initialize(max_capacity = 10)
+    @max_capacity = max_capacity
+    @cargo_contents = []
   end
 
+  def add_cargo(item)
+    raise 'No space available for more cargo' if cargo_contents.size >= max_capacity
+
+    cargo_contents << item
+  end
+
+  def remove_cargo(item)
+    cargo_contents.delete(item)
+  end
+
+  def remaining_capacity
+    max_capacity - cargo_contents.size
+  end
 end
--- main.rb
+++ main.rb
@@ -1,13 +1,19 @@
-!# /usr/bin/env ruby
+#! /usr/bin/env ruby
 
 require_relative 'bike'
 
-bike = Bike.new(1, :pink, 99.99)
+# Create a cargo storage instance with a default capacity of 10 items
+cargo_storage = CargoStorage.new
 
-bike.add_cargo(:apples)
-bike.add_cargo(:water)
-bike.add_cargo(:repair_kit)
+# Instantiate a Bike with the cargo storage
+bike = Bike.new(1, :pink, 99.99, cargo_storage)
+
+# Add cargo items
+bike.cargo_storage.add_cargo(:apples)
+bike.cargo_storage.add_cargo(:water)
+bike.cargo_storage.add_cargo(:repair_kit)
 
 puts "Space for #{bike.pannier_remaining_capacity} items left."
 
+# Rent the bike
 bike.rent!
